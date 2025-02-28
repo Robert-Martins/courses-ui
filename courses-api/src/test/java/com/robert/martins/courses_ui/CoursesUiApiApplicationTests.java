e1376b346728d043e40226c5f512321257fc06a1
Robert-Martins <robertcantares@gmail.com>
2025-02-28 09:59:35 -0300

    feat/start-front-and-back-end-applications-projects

---
+++ courses-api/src/test/java/com/robert/martins/courses_ui/CoursesUiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robert.martins.courses_ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesUiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
