93f2da3d5570f13cff033627693f6544e51bde38
Robert-Martins <robertcantares@gmail.com>
2025-03-06 11:32:49 -0300

    feat/map-student-entity-and-dto

---
+++ courses-api/src/main/java/com/robert/martins/courses_ui/domain/models/Aluno.java
@@ -0,0 +1,48 @@
+package com.robert.martins.courses_ui.domain.models;
+
+import com.robert.martins.courses_ui.infrastructure.base.BaseEntity;
+import com.robert.martins.courses_ui.infrastructure.vo.enums.SituacaoAluno;
+import com.robert.martins.courses_ui.presentation.dtos.AlunoDto;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@DynamicInsert
+@DynamicUpdate
+@Entity
+@Table(name = "tb_alunos")
+public class Aluno extends BaseEntity {
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "data_nascimento")
+    private LocalDate dataNascimento;
+
+    @Column(name = "cpf")
+    private String cpf;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "situacao")
+    private SituacaoAluno situacao;
+
+    @Override
+    public AlunoDto mapEntityToDto() {
+        AlunoDto alunoDto = new AlunoDto();
+        alunoDto.setId(this.getId());
+        alunoDto.setNome(this.getNome());
+        alunoDto.setDataNascimento(this.getDataNascimento());
+        alunoDto.setCpf(this.getCpf());
+        alunoDto.setSituacao(this.getSituacao());
+        alunoDto.setUpdatedAt(this.getUpdatedAt());
+        alunoDto.setCreatedAt(this.getCreatedAt());
+        return alunoDto;
+    }
+
+}
---
+++ courses-api/src/main/java/com/robert/martins/courses_ui/presentation/dtos/AlunoDto.java
@@ -0,0 +1,47 @@
+package com.robert.martins.courses_ui.presentation.dtos;
+
+import com.robert.martins.courses_ui.domain.models.Aluno;
+import com.robert.martins.courses_ui.infrastructure.base.BaseEntityDto;
+import com.robert.martins.courses_ui.infrastructure.vo.enums.SituacaoAluno;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class AlunoDto extends BaseEntityDto<Aluno> {
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 10, max = 100)
+    private String nome;
+
+    @NotNull
+    private LocalDate dataNascimento;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 11, max = 11)
+    private String cpf;
+
+    @NotNull
+    private SituacaoAluno situacao;
+
+
+    @Override
+    public Aluno mapDtoToEntity() {
+        Aluno aluno = new Aluno();
+        aluno.setId(this.getId());
+        aluno.setNome(this.getNome());
+        aluno.setDataNascimento(this.getDataNascimento());
+        aluno.setCpf(this.getCpf());
+        aluno.setSituacao(this.getSituacao());
+        aluno.setUpdatedAt(this.getUpdatedAt());
+        aluno.setCreatedAt(this.getCreatedAt());
+        return aluno;
+    }
+}
