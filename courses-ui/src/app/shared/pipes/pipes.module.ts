1a9e864faa371f5981aa3088b4ea87a339356586
Robert-Martins <robertcantares@gmail.com>
2025-03-07 11:40:38 -0300

    feat/implement-needed-pipes

--- courses-ui/src/app/shared/pipes/pipes.module.ts
+++ courses-ui/src/app/shared/pipes/pipes.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DateformatPipe } from './dateformat.pipe';
+import { NamecasePipe } from './namecase.pipe';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    DateformatPipe,
+    NamecasePipe
+  ],
+  exports: [
+    DateformatPipe,
+    NamecasePipe
   ]
 })
 export class PipesModule { }
