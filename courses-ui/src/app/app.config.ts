a3f52dd0b4bb68d0438c85e89017da782ad69dc8
Robert-Martins <robertcantares@gmail.com>
2025-03-14 00:21:21 -0300

    feat/refactor-configs

--- courses-ui/src/app/app.config.ts
+++ courses-ui/src/app/app.config.ts
@@ -1,22 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { MAT_DATE_FORMATS, MAT_DATE_LOCALE, MatDateFormats, provideNativeDateAdapter } from '@angular/material/core';
+import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
-export const CUSTOM_DATE_FORMATS: MatDateFormats = {
-  parse: {
-    dateInput: 'YYYY-MM-DD HH:mm:ss',
-  },
-  display: {
-    dateInput: 'YYYY-MM-DD HH:mm:ss',
-    monthYearLabel: 'MMMM YYYY',
-    dateA11yLabel: 'LL',
-    monthYearA11yLabel: 'MMMM YYYY',
-  }
-};
+registerLocaleData(localePt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -26,6 +18,6 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withFetch()),
     provideNativeDateAdapter(),
     { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
-    { provide: MAT_DATE_FORMATS, useValue: CUSTOM_DATE_FORMATS }
+    { provide: LOCALE_ID, useValue: 'pt-BR' } 
   ]
 };
